3406ea3c76b534d2aa250e2a0feb51b3350dfc06
Jens Finkhaeuser <jens@finkhaeuser.de>
2016-12-13 14:06:44 +0100

    Implementation of the Test::Error event

---
+++ lib/octiron-test/version.rb
@@ -0,0 +1,16 @@
+# coding: utf-8
+#
+# octiron-test
+# https://github.com/jfinkhaeuser/octiron-test
+#
+# Copyright (c) 2016 Jens Finkhaeuser and other octiron contributors.
+# All rights reserved.
+#
+module Octiron
+  module EventDefinitions
+    module Test
+      # The current release version
+      VERSION = "0.1.0".freeze
+    end # module Test
+  end # module EventDefinitions
+end # module Octiron
